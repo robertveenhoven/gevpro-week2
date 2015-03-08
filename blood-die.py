4cbdad03974e0a8a4dece903b0eaeeffe43e2355
robertveenhoven <robert-veenhoven@hotmail.com>
2015-03-08 12:33:48 -0700

    split verandert in spatie

--- blood-die.py
+++ blood-die.py
@@ -11,8 +11,8 @@ def main():
 	matching = namedtuple('matching', ('language', 'classification', 'blood', 'die'))
 	for language in file:
 		result = matching(language[0],language[1],language[2],language[3])
-		blood = result.blood.split(",")
-		die = result.die.split(",")
+		blood = result.blood.split()
+		die = result.die.split()
 		[json.dump(language,output) for i in blood if i in die]
 	
 if __name__ == "__main__":
